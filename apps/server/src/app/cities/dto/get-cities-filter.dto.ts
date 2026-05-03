bf69da46a6e634335d0979a85f1e59946d9b70a5
Daria Stepanova <daria.stepanova@adesso.de>
2026-05-03 21:32:07 +0200

    add swager to the search param

--- apps/server/src/app/cities/dto/get-cities-filter.dto.ts
+++ apps/server/src/app/cities/dto/get-cities-filter.dto.ts
@@ -1,7 +1,12 @@
 import { IsOptional, IsString } from 'class-validator';
 import { Transform } from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class GetCitiesFilterDto {
+  @ApiPropertyOptional({
+    description: 'Search by city name, country, continent or the city landmarks',
+    example: 'Australia'
+  })
   @IsOptional()
   @IsString()
   @Transform(({value}) => value?.trim())
