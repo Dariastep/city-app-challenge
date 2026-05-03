e5635bdd7027b7aa3a64e034843bff5f7804e83a
Daria Stepanova <daria.stepanova@adesso.de>
2026-05-03 14:29:30 +0200

    add safe data mapping

--- lib/shared/src/lib/city-raw.model.ts
+++ lib/shared/src/lib/city-raw.model.ts
@@ -1,11 +1,11 @@
 export interface CityRaw {
-  name: string;
-  name_native: string;
-  country: string;
-  continent: string;
-  latitude: string;
-  longitude: string;
-  population: string;
-  founded: string;
-  landmarks: string[];
+  name?: string;
+  name_native?: string;
+  country?: string;
+  continent?: string;
+  latitude?: string | number;
+  longitude?: string | number;
+  population?: string | number;
+  founded?: string | number;
+  landmarks?: string[];
 }
--- lib/shared/src/lib/city.mapper.ts
+++ lib/shared/src/lib/city.mapper.ts
@@ -8,14 +8,14 @@ import { City } from './city.model';
  */
 export function mapCityRawToCity(data: CityRaw): City {
   return {
-  name: data.name,
-  nameNative: data.name_native,
-  country: data.country,
-  continent: data.continent,
-  latitude: Number(data.latitude),
-  longitude: Number(data.longitude),
-  population: Number(data.population),
-  founded: Number(data.founded),
-  landmarks: data.landmarks,
-  }
+    name: data.name ?? 'unknown city',
+    nameNative: data.name_native ?? data.name ?? 'unknown city',
+    country: data.country ?? 'unknown country',
+    continent: data.continent ?? 'unknown continent',
+    latitude: Number(data.latitude) || 0,
+    longitude: Number(data.longitude) || 0,
+    population: parseInt(data.population?.toString() ?? '', 10) || 0,
+    founded: parseInt(data.founded?.toString() ?? '', 10) || 0,
+    landmarks: Array.isArray(data.landmarks) ? data.landmarks : [],
+  };
 }
