34271a85fb12084ec967fac20a3719c2f9fd270f
Daria Stepanova <daria.stepanova@adesso.de>
2026-05-03 15:38:31 +0200

    bind a filterDto

--- apps/server/src/app/cities/cities.controller.ts
+++ apps/server/src/app/cities/cities.controller.ts
@@ -1,15 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { CitiesService } from './cities.service';
 import { City } from '../../../../../lib/shared/src/index';
+import { GetCitiesFilterDto } from './dto/get-cities-filter.dto';
 
 
 @Controller('cities')
 export class CitiesController {
-  constructor( private citiesService: CitiesService ) {}
+  constructor(private citiesService: CitiesService) {}
 
-@Get()
-    getAllCities(): City[] {
-      return this.citiesService.getCities();
-    }
-
+  @Get()
+  getAllCities(@Query() getCitiesFilterDto: GetCitiesFilterDto): City[] {
+    return this.citiesService.getCities(getCitiesFilterDto);
+  }
 }
--- apps/server/src/app/cities/cities.service.ts
+++ apps/server/src/app/cities/cities.service.ts
@@ -1,10 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  LoggerService,
+} from '@nestjs/common';
 import { cities } from '../../assets/data/cities.json';
-import { City, CityRaw, mapCityRawToCity } from '../../../../../lib/shared/src/index';
+import {
+  City,
+  CityRaw,
+  mapCityRawToCity,
+} from '../../../../../lib/shared/src/index';
+import { GetCitiesFilterDto } from './dto/get-cities-filter.dto';
 
 @Injectable()
 export class CitiesService {
-  getCities(): City[] {
-    return cities.map((city: CityRaw) => mapCityRawToCity(city));
+  private readonly logger = new Logger(CitiesService.name);
+
+  getCities(filterDto: GetCitiesFilterDto): City[] {
+    try {
+      const mappedCity = cities.map((city: CityRaw) => mapCityRawToCity(city));
+
+      if (!filterDto.search) {
+        return mappedCity;
+      }
+
+      const query = filterDto.search?.trim().toLowerCase();
+
+      const result = mappedCity.filter(
+        (city) =>
+          city.name.toLowerCase().includes(query) ||
+          city.country.toLowerCase().includes(query) ||
+          city.continent.toLowerCase().includes(query) ||
+          city.landmarks.some((landmark) =>
+            landmark.toLowerCase().includes(query),
+          ),
+      );
+
+      return result;
+    } catch (error) {
+      this.logger.error('Failed to get the cities', error);
+
+      throw new InternalServerErrorException();
+    }
   }
- }
+}
