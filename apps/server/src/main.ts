4bfc84f8ec80a3e4eeb7f7638e11fe1bb516a39b
Daria Stepanova <daria.stepanova@adesso.de>
2026-05-03 21:31:43 +0200

    add swager and setup

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,16 +1,32 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe( {transform: true }));
+  app.enableCors({
+    origin: 'http://localhost:4200',
+  });
+  const config = new DocumentBuilder()
+    .setTitle('City App')
+    .setDescription(
+      'The API to explore the cities and search by city name, countries, continents and the city landmarks.',
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
+  Logger.log(
+    `Swagger is running on: http://localhost:${port}/api-docs`,
   );
 }
 
