8646eaf672959f74226cd3c24a5c0a0584265647
Daria Stepanova <daria.stepanova@adesso.de>
2026-05-04 16:38:14 +0200

    create city details component

---
+++ apps/client/src/app/components/city-details/city-details-component.spec.ts
@@ -0,0 +1,59 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CityDetailsComponent } from './city-details-component';
+import { City } from '@lib/shared';
+import { provideRouter } from '@angular/router';
+import { CityApiService } from '../../services/city-api.service';
+import { of } from 'rxjs';
+
+describe('CityDetails', () => {
+  let component: CityDetailsComponent;
+  let fixture: ComponentFixture<CityDetailsComponent>;
+
+  const mockedCity: City = {
+    id: '7b686d1a-1d5d-4f1e-9c1a-1234567890ab',
+    name: 'Madrid',
+    nameNative: 'Madrid',
+    country: 'Spain',
+    continent: 'Europe',
+    latitude: 40.416775,
+    longitude: -3.70379,
+    population: 3223000,
+    founded: 1083,
+    landmarks: ['Royal Palace', 'Plaza Mayor', 'Plaza de Cibeles'],
+  };
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CityDetailsComponent],
+      providers: [provideRouter([]),
+        {
+          provide: CityApiService,
+          useValue: { getCityById: () => of(mockedCity) }
+        }]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CityDetailsComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render city data', () => {
+    fixture.componentRef.setInput('id', '7b686d1a-1d5d-4f1e-9c1a-1234567890ab');
+    fixture.detectChanges();
+
+    const content = fixture.nativeElement.textContent;
+
+    expect(content).toContain(mockedCity.name);
+    expect(content).toContain(mockedCity.nameNative);
+    expect(content).toContain(mockedCity.continent);
+    expect(content).toContain('3,223,000');
+    expect(content).toContain(mockedCity.longitude.toString());
+    expect(content).toContain(mockedCity.latitude.toString());
+
+    mockedCity.landmarks.forEach((landmark) => expect(content).toContain(landmark));
+  });
+});
---
+++ apps/client/src/app/components/city-details/city-details-component.ts
@@ -0,0 +1,51 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+} from '@angular/core';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { MatDivider, MatList, MatListItem } from '@angular/material/list';
+import { DecimalPipe } from '@angular/common';
+import { CityApiService } from '../../services/city-api.service';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { filter, switchMap } from 'rxjs';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { RouterLink } from '@angular/router';
+import { MatButton } from '@angular/material/button';
+
+@Component({
+  selector: 'city-details',
+  imports: [
+    MatCardHeader,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatIcon,
+    MatDivider,
+    MatCardContent,
+    MatCardActions,
+    DecimalPipe,
+    MatCard,
+    MatProgressBar,
+    MatList,
+    MatListItem,
+    RouterLink,
+    MatButton,
+  ],
+  templateUrl: './city-details-component.html',
+  styleUrl: './city-details-component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CityDetailsComponent {
+  private readonly cityApiService = inject(CityApiService);
+
+  id = input<string>();
+
+  city = toSignal(
+    toObservable(this.id).pipe(
+      filter((id): id is string => !!id),
+      switchMap((id) => this.cityApiService.getCityById(id)),
+    ),
+  );
+}
