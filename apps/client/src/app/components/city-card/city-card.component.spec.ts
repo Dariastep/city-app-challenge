a5e2ce6963b628324239c2e39e423f0868abdf60
Daria Stepanova <daria.stepanova@adesso.de>
2026-05-04 17:37:51 +0200

    create city card component

---
+++ apps/client/src/app/components/city-card/city-card.component.spec.ts
@@ -0,0 +1,38 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CityCardComponent } from './city-card.component';
+import { provideRouter, RouterLink } from '@angular/router';
+import { By } from '@angular/platform-browser';
+
+describe('CityCard', () => {
+  let component: CityCardComponent;
+  let fixture: ComponentFixture<CityCardComponent>;
+
+  const mockedCity = {
+    id: '7b686d1a-1d5d-4f1e-9c1a-1234567890ab',
+    name: 'Madrid',
+    country: 'Spain',
+  };
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CityCardComponent],
+      providers: [provideRouter([])]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CityCardComponent);
+    component = fixture.componentInstance;
+    fixture.componentRef.setInput('city', mockedCity);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+
+  it('should have routerLink directive', () => {
+    const el = fixture.debugElement.query(By.directive(RouterLink));
+
+    expect(el).toBeTruthy();
+  });
+});
---
+++ apps/client/src/app/components/city-card/city-card.component.ts
@@ -0,0 +1,26 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { City } from '../../../../../../lib/shared/src/lib/models/city.model';
+import { RouterLink } from '@angular/router';
+import { MatCard, MatCardActions, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'city-card',
+  imports: [
+    RouterLink,
+    MatCard,
+    MatButton,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardActions,
+    MatIcon,
+  ],
+  templateUrl: './city-card.component.html',
+  styleUrl: './city-card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CityCardComponent {
+  city = input.required<City>();
+}
