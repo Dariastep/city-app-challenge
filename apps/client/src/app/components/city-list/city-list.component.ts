288d67a721a7b403232231c943ec06e9bf6f7d53
Daria Stepanova <daria.stepanova@adesso.de>
2026-05-04 18:13:16 +0200

    implement searchControl

--- apps/client/src/app/components/city-list/city-list.component.ts
+++ apps/client/src/app/components/city-list/city-list.component.ts
@@ -3,10 +3,23 @@ import { CommonModule } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { CityCardComponent } from '../city-card/city-card.component';
 import { CityApiService } from '../../services/city-api.service';
+import { MatFormField, MatHint, MatInput, MatLabel } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, startWith, switchMap } from 'rxjs';
 
 @Component({
   selector: 'city-list',
-  imports: [CommonModule, CityCardComponent],
+  imports: [
+    CommonModule,
+    CityCardComponent,
+    MatFormField,
+    MatLabel,
+    MatIcon,
+    MatInput,
+    ReactiveFormsModule,
+    MatHint,
+  ],
   templateUrl: './city-list.component.html',
   styleUrl: './city-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,5 +27,15 @@ import { CityApiService } from '../../services/city-api.service';
 export class CityListComponent {
   private readonly cityApiService = inject(CityApiService);
 
-  cities = toSignal(this.cityApiService.getAllCities(), { initialValue: [] });
+  searchControl = new FormControl('', { nonNullable: true });
+
+  cities = toSignal(
+    this.searchControl.valueChanges.pipe(
+      startWith(''),
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((search) => this.cityApiService.getAllCities(search)),
+    ),
+    { initialValue: [] },
+  );
 }
--- apps/client/src/app/services/city-api.service.ts
+++ apps/client/src/app/services/city-api.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { City } from '@lib/models';
 import { catchError, Observable, throwError } from 'rxjs';
 
@@ -9,18 +9,28 @@ export class CityApiService {
 
   private readonly apiUrl = 'http://localhost:3000/api/cities';
 
-  getAllCities(): Observable<City[]> {
-    return this.httpClient.get<City[]>(this.apiUrl).pipe(catchError(this.handleError));
+  getAllCities(search?: string): Observable<City[]> {
+    let params = new HttpParams();
+    if (search) {
+      params = params.set('search', search);
+    }
+
+    return this.httpClient
+      .get<City[]>(this.apiUrl, {params})
+      .pipe(catchError(this.handleError));
   }
 
   getCityById(id: string): Observable<City> {
-    return this.httpClient.get<City>(`${this.apiUrl}/${id}`).pipe(catchError(this.handleError));
+    return this.httpClient
+      .get<City>(`${this.apiUrl}/${id}`)
+      .pipe(catchError(this.handleError));
   }
 
   private handleError(error: HttpErrorResponse) {
     let message = 'An unexpected error occurred.';
     if (error.status === 404) message = 'City not found.';
-    if (error.status === 0) message = 'Network error. Please contact your administrator';
+    if (error.status === 0)
+      message = 'Network error. Please contact your administrator';
 
     return throwError(() => new Error(message));
   }
